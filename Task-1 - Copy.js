8c5ba40aaa4c56552d749f5c1bf82e4ed35cf913
AditiMuskan <159609924+AditiMuskan@users.noreply.github.com>
2024-02-11 13:44:07 +0530

    Add files via upload

---
+++ Task-1 - Copy.js
@@ -0,0 +1,7 @@
+// You can add JavaScript code here for interactivity
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-menu_visible');
+});
